5941e68523d9f482e5e5691b85c5ad0d4f8329f3
a325g <avideogino@gmail.com>
2026-02-04 15:22:28 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a32x.mk
+    $(LOCAL_DIR)/lineage_a32x.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_a32x-user \
-    twrp_a32x-userdebug \
-    twrp_a32x-eng
+    lineage_a32x-user \
+    lineage_a32x-userdebug \
+    lineage_a32x-eng
